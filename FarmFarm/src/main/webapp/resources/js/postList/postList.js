3426a3ea1eed9d4c95c4cf728169d7370a5c0c70
cymakp <chimakpi0804@gmail.com>
2022-12-18 11:46:40 +0900

    1218 nav바 스크롤 시 검색창 추가

--- FarmFarm/src/main/webapp/resources/js/postList/postList.js
+++ FarmFarm/src/main/webapp/resources/js/postList/postList.js
@@ -4,20 +4,42 @@ const body = document.getElementsByName("body");
 /* top 버튼 */
 const topBtn = document.getElementById("topBtn");
 
-/* top 버튼 보이기 */
+/* nav 부착 검색창 추가하기 */
+const navSearchBar = document.getElementById("navSearchBar");
+
+/* top 버튼 및 검색창 보이기 */
 window.addEventListener("scroll", ()=> {
-    if(topBtn.classList.contains('view-hidden')) {
+
+    if(navSearchBar.classList.contains('view-hidden')) {
+        if(window.scrollY >= 720) {
+            navSearchBar.classList.remove('view-hidden');
+            navSearchBar.classList.add('view-flex');
+            console.log("실행되고있나요2");
+            return;
+        }
+    }
+
+    if(navSearchBar.classList.contains('view-flex')) {
+        if(window.scrollY < 720) {
+            navSearchBar.classList.add('view-hidden');
+            navSearchBar.classList.remove('view-flex');
+            console.log("실행되고있나요2");
+            return;
+        }
+    }
+
+    if(topBtn.classList.contains('opacity-zero')) {
         if(window.scrollY >= 1000) {
-            topBtn.classList.remove('view-hidden');
-            topBtn.classList.add('view-flex');
+            topBtn.classList.remove('opacity-zero');
+            topBtn.classList.add('opacity-one');
             console.log("실행되고있나요");
             return;
         }
     }
-    if(topBtn.classList.contains('view-flex')) {
+    if(topBtn.classList.contains('opacity-one')) {
         if(window.scrollY < 1000) {
-            topBtn.classList.add('view-hidden');
-            topBtn.classList.remove('view-flex');
+            topBtn.classList.add('opacity-zero');
+            topBtn.classList.remove('opacity-one');
             console.log("실행되고있나요");
             return;
         }
@@ -26,9 +48,9 @@ window.addEventListener("scroll", ()=> {
 
 /* top 버튼 클릭 시 맨 위로 */
 topBtn.addEventListener("click", ()=>{
-    if(topBtn.classList.contains('view-flex')) {
-        topBtn.classList.add('view-hidden');
-        topBtn.classList.remove('view-flex');
+    if(topBtn.classList.contains('opacity-one')) {
+        topBtn.classList.add('opacity-zero');
+        topBtn.classList.remove('opacity-one');
         scrollTo(0,0);
     }
 })
