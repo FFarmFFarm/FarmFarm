bf06cb4c19b3cd910476ca319a3ad2ef2a093602
97siwon <tldnjs5757@naver.com>
2022-12-18 16:15:37 +0900

    로그인

--- FarmFarm/src/main/java/edu/kh/farmfarm/member/controller/MemberController.java
+++ FarmFarm/src/main/java/edu/kh/farmfarm/member/controller/MemberController.java
@@ -1,9 +1,17 @@
 package edu.kh.farmfarm.member.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import edu.kh.farmfarm.member.model.VO.Member;
 import edu.kh.farmfarm.member.model.service.MemberService;
 
 @Controller
@@ -15,6 +23,23 @@ public class MemberController {
 	@GetMapping("/login")
 	public String loginPage() {
 		return "member/login";
+	}
+	
+	// 로그인 하기
+	@PostMapping("/login")
+	public String login(Member inputMember,
+						Model model,
+						RedirectAttributes ra,
+						@RequestHeader(value="referer") String referer,
+						@RequestParam(value="saveId", required = false) String saveId,
+						HttpServletResponse resp) {
+		Member loginMember = service.login(inputMember);
+		
+		String path = null;
+		
+		
+		
+		return null;
 	}
 
 }
