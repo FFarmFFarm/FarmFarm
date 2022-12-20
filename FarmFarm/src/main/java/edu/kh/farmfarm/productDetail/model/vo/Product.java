09301463d05abb75cd4071f2449d0ef4809ba1f3
nanazzoo <bboya0518@naver.com>
2022-12-20 12:05:28 +0900

    20221220 vo 생성

--- FarmFarm/src/main/java/edu/kh/farmfarm/productDetail/model/vo/Product.java
+++ FarmFarm/src/main/java/edu/kh/farmfarm/productDetail/model/vo/Product.java
@@ -1,5 +1,22 @@
 package edu.kh.farmfarm.productDetail.model.vo;
 
+import java.util.List;
+
 public class Product {
+	
+	private int productNo;
+	private String productName;
+	private String productContent;
+	private String productMessage;
+	private String regDate;
+	private int productPrice;
+	private String productDelFl;
+	private int memberNo;
+	private int categoryNo;
+	private int stock;
+	
+	private List<ProductImg> imgList;
+	
+	
 
 }
---
+++ FarmFarm/src/main/java/edu/kh/farmfarm/productDetail/model/vo/ProductImg.java
@@ -0,0 +1,10 @@
+package edu.kh.farmfarm.productDetail.model.vo;
+
+public class ProductImg {
+	private int productImgNo;
+	private String productImgAddress;
+	private int productNo;
+	private int productImgOrder;
+	
+
+}
