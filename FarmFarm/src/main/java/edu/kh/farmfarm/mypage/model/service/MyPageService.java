edabe22c201681e5f17721a2040637d42a07f870
nanazzoo <bboya0518@naver.com>
2022-12-21 00:12:54 +0900

    20221220 마이페이지 댓글 목록 조회

--- FarmFarm/src/main/java/edu/kh/farmfarm/mypage/model/service/MyPageService.java
+++ FarmFarm/src/main/java/edu/kh/farmfarm/mypage/model/service/MyPageService.java
@@ -1,5 +1,12 @@
 package edu.kh.farmfarm.mypage.model.service;
 
+import java.util.List;
+import java.util.Map;
+
+import edu.kh.farmfarm.mypage.model.vo.Comment;
+
 public interface MyPageService {
+
+	Map<String, Object> selectCommentList(int memberNo, int cp);
 
 }
