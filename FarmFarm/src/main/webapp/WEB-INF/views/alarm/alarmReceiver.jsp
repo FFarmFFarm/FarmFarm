9af7f39dadaa859a9fd6576d46ce2786f6907922
cymakp <chimakpi0804@gmail.com>
2022-12-31 02:04:51 +0900

    1231 알림 기능 방식 추가

--- FarmFarm/src/main/webapp/WEB-INF/views/alarm/alarmReceiver.jsp
+++ FarmFarm/src/main/webapp/WEB-INF/views/alarm/alarmReceiver.jsp
@@ -5,26 +5,32 @@
 <link rel="stylesheet" href="/resources/css/alarm/alarmReceiver.css">
 <script src="https://kit.fontawesome.com/d449774bd8.js" crossorigin="anonymous"></script>
 
+
+<a id="alarmReceiverArea">
+
 <a class="alarm-receiver-container">
 
-    <div class="alarm-receiver-header">
+    <!-- <div class="alarm-receiver-header"> -->
     
-        <div class="alarm-receiver-icon">
-            <i class="fa-solid fa-bell"></i>
-        </div> <!-- end alarm-icon -->
+        <!-- <div class="alarm-receiver-icon"> -->
+            <!-- <i class="fa-solid fa-bell"></i> -->
+        <!-- </div> end alarm-icon -->
 
-        <div class="alarm-receiver-title">
-            <!-- 댓글 유형 제목 영역(예:새로운 댓글이 달렸어요) --> 
-        </div> <!-- end alarm-title -->
+        <!-- <div class="alarm-receiver-title"> -->
+            <!-- 알림 유형 제목 영역 -->
+            <!-- 예 : 새로운 댓글이 달렸어요 --> 
+        <!-- </div> end alarm-title -->
     
-    </div> <!-- end alarm-receiver-header -->
+    <!-- </div> end alarm-receiver-header -->
 
-        <div class="alarm-receiver-content">
-            <!-- 댓글 영역 -->
-        </div> <!-- end alarm-receiver-content -->
+        <!-- <div class="alarm-receiver-content"> -->
+            <!-- 알림 유형 상세내용 영역 -->
+            <!-- 예 : 댓글 내용 -->
+        <!-- </div> end alarm-receiver-content -->
 
 </a> <!-- end alarm-reveiver-container -->
 
+</a>
 <!-- https://github.com/sockjs/sockjs-client -->
 <script src="https://cdn.jsdelivr.net/npm/sockjs-client@1/dist/sockjs.min.js"></script>
 
--- FarmFarm/src/main/webapp/resources/js/alarm/alarmReceiver.js
+++ FarmFarm/src/main/webapp/resources/js/alarm/alarmReceiver.js
@@ -33,7 +33,8 @@ addEventListener("DOMContentLoaded", ()=>{
                 // 알림 내용이 있을 때에만, 수신 알림 처리 함수를 선언
                 if(alarm != null) {
                     // 수신 알림 처리 함수인 fillAlarmContainer 실행해서 받은 데이터를 전송
-                    fillAlarmReceiverContainer(alarm);
+                    // fillAlarmReceiverContainer(alarm);
+                    createAlarmReceiverContainer(alarm);
                 }
             }
         }
@@ -52,11 +53,15 @@ addEventListener("DOMContentLoaded", ()=>{
     1) 알림이 여러 개 왔을 때, 어떻게 처리할 것인지
 */
 
+/*   
+    * 방식 1: JSP에 미리 준비해둔 요소를 채우고 노출시키는 방식
+    - 예상되는 장점 : 빠름
+    - 예상되는 단점 : 알림이 여러 개 왔을 때, 노출되고 있던 알림 내용이 갑자기 바뀌는 등 깔끔하게 처리되지 않을 수 있음
+*/
 const fillAlarmReceiverContainer = (alarm) => {
 
-    // 알림 전, 만약 노출된 alarm-conatiner가 있는 경우 노출된 alarm-container를 다시 숨김
-    clearTimeout(hidetimer);
-
+    // 알림 전, 만약 노출된 alarm-receiver-conatiner가 있는 경우 노출된 alarm-receiver-container를 다시 숨김
+    clearTimeout(hidetimer); // timeout 이벤트 제거
     hideAlarm(); // 즉시 숨김
 
     // alarm-receiver-container의 요소
@@ -68,8 +73,7 @@ const fillAlarmReceiverContainer = (alarm) => {
     alarmReceiverIcon.innerHTML = "";
     alarmReceiverTitle.innerHTML = "";
     alarmReceiverContent.innerHTML = "";
-
-    console.log(alarm.alarmTypeNo);
+    
 
     // 알림 아이콘 채우기
     switch(alarm.alarmTypeNo){
@@ -86,24 +90,84 @@ const fillAlarmReceiverContainer = (alarm) => {
     // 알림 컨테이너에 링크(quickLink) 세팅하기
     document.querySelector('.alarm-receiver-container').setAttribute('href', alarm.quickLink);
 
-    // 내용을 채운 후, 잠시 딜레이 후, 숨어있던 alarm-container를 노출
+    // 내용을 채운 후, 잠시 딜레이 후, 숨어있던 alarm-receiver-container를 노출
     pushtimer = setTimeout(pushAlarm, 1000) // 1초 후 노출
-    
-    // 노출 후, 잠시 딜레이 후, 노출된 alarm-container를 다시 숨김
+
+    // 노출 후, 잠시 딜레이 후, 노출된 alarm-receiver-container를 다시 숨김
     hidetimer = setTimeout(hideAlarm, 5000) // 5초 후 숨김
 
 }
 
-/* alarm-container를 숨김 해제하는 함수 */
-const pushAlarm = () => {
-    const alarmReceiverContainer = document.querySelector('.alarm-receiver-container');
-    alarmReceiverContainer.style.opacity='1';
-    alarmReceiverContainer.style.height = '150px';
-}
+/* 
+    * 방식 2 : 알림이 올 때마다, alarm-receiver-continaer를 새로 만들어서 append하고, 알림이 끝나면 통째로 지우는 방식
+    - 예상되는 장점 : 알림이 여러 개 오더라도 깔끔하게 처리 가능
+    - 예상되는 장점 : 느림, 성능 문제 우려
+*/
+// const createAlarmReceiverContainer = (alarm) => {
 
-/* alarm-container를 숨기는 함수 */
-const hideAlarm = () => {
-    const alarmReceiverContainer = document.querySelector('.alarm-receiver-container');
-    alarmReceiverContainer.style.opacity='0';
-    alarmReceiverContainer.style.height = '0px';
-}
+//     // 알림 전, 만약 노출된 alarm-receiver-conatiner가 있는 경우 노출된 alarm-receiver-container를 다시 숨김
+
+//     // alarm-receiver-container의 요소
+//     const alarmReceiverIcon = document.createElement('div')
+//     alarmReceiverIcon.classList.add('alarm-receiver-icon'); // 알림 아이콘
+//     const alarmReceiverTitle = document.createElement('div')
+//     alarmReceiverTitle.classList.add('alarm-receiver-title'); // 알림 제목(== 알림 유형)
+//     const alarmReceiverContent = document.createElement('div')
+//     alarmReceiverTitle.classList.add('alarm-receiver-content'); // 알림 상세 내용
+    
+
+//     // 알림 아이콘 채우기
+//     switch(alarm.alarmTypeNo){
+//         case 201:  alarmReceiverIcon.innerHTML = "<i class='fa-regular fa-comment-dots'></i>"; break;
+//         case 202:  alarmReceiverIcon.innerHTML = "<i class='fa-regular fa-comment-dots'></i>"; break;
+//     }
+
+//     // 알림 제목 채우기
+//     alarmReceiverTitle.innerText = alarm.alarmTitle;
+
+//     // 알림 내용 채우기
+//     alarmReceiverContent.innerText = alarm.alarmContent;
+
+//     // 알림 컨테이너에 요소 세팅
+//     const alarmReceiverHeader = document.createElement('div');
+//     alarmReceiverHeader.classList.add('alarm-receiver-header');
+//     alarmReceiverHeader.append(alarmReceiverIcon, alarmReceiverTitle);
+    
+//     const alarmReceiverContainer = document.createElement('a');
+//     alarmReceiverContainer.classList.add('alarm-receiver-container');
+//     alarmReceiverContainer.append(alarmReceiverHeader, alarmReceiverContent);
+    
+//     // 알림 컨테이너에 링크(quickLink) 세팅하기
+//     alarmReceiverContainer.setAttribute('href', alarm.quickLink);
+    
+//     document.getElementById('alarmReceiverArea').append(alarmReceiverContainer);
+
+//     // 내용을 채운 후, 잠시 딜레이 후, 숨어있던 alarm-receiver-container를 노출
+//     pushtimer = setTimeout(pushAlarm, 1000) // 1초 후 노출
+
+//     // 노출 후, 잠시 딜레이 후, 노출된 alarm-receiver-container를 다시 숨김
+//     hidetimer = setTimeout(hideAlarm, 5000) // 5초 후 숨김
+
+//     // 삭제
+//     document.getElementById('alarmReceiverArea').innerHTML = '';
+
+// }
+
+// /* alarm-receiver-container를 숨김 해제하는 함수 */
+// const pushAlarm = () => {
+//     const alarmReceiverContainer = document.querySelector('.alarm-receiver-container');
+//     if(alarmReceiverContainer != null) {
+//         alarmReceiverContainer.style.opacity='1';
+//         alarmReceiverContainer.style.height = '150px';
+//     }
+// }
+
+// /* alarm-receiver-container를 숨기는 함수 */
+// const hideAlarm = () => {
+//     const alarmReceiverContainer = document.querySelector('.alarm-receiver-container');
+//     if(alarmReceiverContainer != null) {
+//         alarmReceiverContainer.style.opacity='0';
+
+//         alarmReceiverContainer.style.height = '0px';
+//     }
+// }
