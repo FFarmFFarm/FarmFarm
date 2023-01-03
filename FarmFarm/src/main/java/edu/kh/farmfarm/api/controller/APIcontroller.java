98fd4e6e008758373827aef9d980f5df6377afa1
97siwon <tldnjs5757@naver.com>
2023-01-03 17:15:17 +0900

    main

--- FarmFarm/src/main/java/edu/kh/farmfarm/api/controller/APIcontroller.java
+++ FarmFarm/src/main/java/edu/kh/farmfarm/api/controller/APIcontroller.java
@@ -1,5 +1,17 @@
-package edu.kh.farmfarm.api.controller;
-
-public class APIcontroller {
-
-}
+//package edu.kh.farmfarm.api.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class APIcontroller {
+//	@GetMapping("/")
+//	public String index(Model m) throws Exception{
+//		StringBuffer sb = new StringBuffer(http://211.237.50.150:7080/openapi/sample/xml/Grid_20171128000000000572_1/1/20);
+//		
+//		
+//		return null;
+//	}
+//	
+//}
