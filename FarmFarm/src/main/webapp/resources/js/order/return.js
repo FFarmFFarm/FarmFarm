e2f10600ae67e8ac7038b0e9c0249cd711e09ac2
nanazzoo <bboya0518@naver.com>
2023-01-09 22:36:28 +0900

    20230109 반품등록 유효성검사, 택배조회 수정

--- FarmFarm/src/main/webapp/resources/js/order/return.js
+++ FarmFarm/src/main/webapp/resources/js/order/return.js
@@ -6,20 +6,79 @@ function selectAllCheckbox(selectAll) {
   }
 }
 
-const returnSubmit = () => {
+
+var checkObj = {
+  'checkbox': false,
+  'returnReason': false,
+  'accountName': false,
+  'accountNo': false
+};
+
+const returnSubmit = (event) => {
 
   const form = document.getElementById('returnForm');
-
-
   const checkbox = document.getElementsByClassName('productNo');
+
+
+  // 체크박스중 하나라도 선택돼있으면 key = true
+  for (let box of checkbox) {
+    if (box.checked) {
+      checkObj['checkbox'] = true;
+    }
+  }
+
+  // 반품 사유 선택돼있으면 true
+  const returnReason = document.getElementsByName("returnReason");
+  for (let radio of returnReason) {
+    if (radio.checked) {
+      checkObj['returnReason'] = true;
+    }
+  }
+
+  // 예금주 입력 돼있으면 true
+  const accountName = document.getElementById("accountName");
+  console.log(accountName);
+  if (accountName.value.trim().length > 0) {
+      checkObj['accountName'] = true; 
+    }
+  
+  // 계좌번호 입력 돼있으면 true
+  const accountNo = document.getElementById("accountNo");
+  if (accountNo.value.trim().length > 0) {
+      checkObj['accountNo'] = true; 
+    }
+
+
+  /* key중 하나라도 false이면 제출 x */
+  for (let key in checkObj) {
+    let str;
+
+    if (!checkObj[key]) { 
+
+      switch (key) {
+        case 'checkbox': str = "최소 한 개 이상의 상품을 선택해주세요"; break;
+        case 'returnReason': str = "반품 사유를 선택해주세요"; break;
+        case 'accountName': str = "예금주명을 입력해주세요"; break;
+        case 'accountNo': str = "계좌번호를 입력해주세요"; break;
+      }
+
+      messageModalOpen(str);
+
+      event.preventDefault();
+      return;
+    }
+  }
+
 
   for (let i = 0; i < checkbox.length; i++) {
 
     if (!checkbox[i].checked) {
 
 
+      // 체크되지 않은 체크박스 제출 X
       checkbox[i].setAttribute('disabled', 'true');
 
+      // 수량 가져와서 제출안되게
       const amount = document.getElementById('amount' + i);
 
       amount.setAttribute('disabled', 'true');
@@ -29,6 +88,8 @@ const returnSubmit = () => {
       console.log(checkbox[i].checked);
     }
 
+
+
   }
 
   setTimeout(() => {
@@ -37,3 +98,12 @@ const returnSubmit = () => {
   }, "1000");
 
 }
+
+
+
+
+// document.getElementById('accountNo').addEventListener('input', e => { 
+
+//   if(e.target.value.)
+
+// })
