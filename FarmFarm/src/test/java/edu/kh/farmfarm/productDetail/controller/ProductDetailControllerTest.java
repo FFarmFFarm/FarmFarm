affa6f3d16da18932969b70ca27caa32570da74d
Shin_Bokyung <bboya0518@naver.com>
2023-03-03 13:23:06 +0900

    20230303 JUnit 라이브러리 추가

---
+++ FarmFarm/src/test/java/edu/kh/farmfarm/productDetail/controller/ProductDetailControllerTest.java
@@ -0,0 +1,29 @@
+package edu.kh.farmfarm.productDetail.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductDetailControllerTest {
+
+	@Test
+	void testMyPageReview() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddWish() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveWish() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testProductStock() {
+		fail("Not yet implemented");
+	}
+
+}
